7274263df5469897d66dc1425699020287b2aed5
iZucked <78699904+iZucked@users.noreply.github.com>
2023-07-11 22:12:43 +0100

    Added models

--- urlshortener/urlapp/admin.py
+++ urlshortener/urlapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import URL, Visitor
 
 # Register your models here.
+admin.register(URL)
+admin.register(Visitor)
--- urlshortener/urlapp/models.py
+++ urlshortener/urlapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Visitor(models.Model):
+    ip_addr = models.GenericIPAddressField()
+    user_agent = models.CharField(max_length=200)
+    visit_time = models.DateTimeField(auto_now=True)
+class URL(models.Model):
+    linked_url = models.URLField(max_length=200)
+    shortened_code = models.CharField(unique=True, max_length=100)
+    creation_time = models.DateTimeField(auto_now=True)
+    visitors = models.ManyToManyField(Visitor, blank=True)
+    qr_code = models.CharField(max_length=500)
+    is_password_protected = models.BooleanField()
+    can_expire = models.BooleanField()
+    password = models.CharField(blank=True, max_length=100)
+    expiration_time = models.DateTimeField(blank=True)
+
